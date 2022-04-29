b1c90e881d7a433603db232cb3db2e05af561179
zerj9 <96551236+zerj9@users.noreply.github.com>
2022-04-29 23:00:36 +0100

    create api construct with auth lambda

---
+++ lib/api.ts
@@ -0,0 +1,37 @@
+import { HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { Architecture, Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import { Construct } from "constructs";
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+
+export interface ApiProps {
+    table: ITable;
+}
+
+export class Api extends Construct {
+    constructor(scope: Construct, id: string, props: ApiProps) {
+        super(scope, id)
+
+        const api = new HttpApi(this, 'HttpApi')
+
+        const authFunction = new Function(this, 'AuthFunction', {
+            description: 'Auth endpoint for use by API Gateway',
+            runtime: Runtime.PROVIDED_AL2,
+            architecture: Architecture.ARM_64,
+            handler: 'not.required',
+            code: Code.fromAsset('functions/auth/target/lambda/auth/bootstrap.zip'),
+            logRetention: RetentionDays.ONE_WEEK,
+            environment: {
+                'RUST_BACKTRACE': '1',
+                'TABLE': props.table.tableName
+            }
+        })
+        props.table.grantReadWriteData(authFunction);
+        api.addRoutes({
+            path: '/auth',
+            methods: [HttpMethod.POST],
+            integration: new HttpLambdaIntegration('AuthIntegration', authFunction)
+        })
+    }
+}
