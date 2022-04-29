a0e20be807bb307ef2c3f5a31eaab3f315cb0bd9
zerj9 <96551236+zerj9@users.noreply.github.com>
2022-04-29 21:31:44 +0100

    create Data construct with DynamoDB and add to stack

--- lib/analytics-platform-stack.ts
+++ lib/analytics-platform-stack.ts
@@ -1,16 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Data } from './data';
 
 export class AnalyticsPlatformStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AnalyticsPlatformQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Data(this, 'Data')
   }
 }
