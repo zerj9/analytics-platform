78da10afaf714672de1e86e306f1c45508108406
zerj9 <96551236+zerj9@users.noreply.github.com>
2022-05-04 00:24:49 +0100

    create custom domain with cert for Api

--- lib/analytics-platform-stack.ts
+++ lib/analytics-platform-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { HostedZone } from 'aws-cdk-lib/aws-route53';
 import { Construct } from 'constructs';
 import { Api } from './api';
 import { Data } from './data';
@@ -7,8 +8,12 @@ export class AnalyticsPlatformStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const hosted_zone = HostedZone.fromLookup(this, 'HostedZoneFromLookup', {
+      domainName: process.env.HOSTED_ZONE as string
+    })
+
     const data = new Data(this, 'Data')
-    new Api(this, 'Api', { table: data.table })
+    new Api(this, 'Api', { table: data.table, hosted_zone: hosted_zone})
 
   }
 }
--- lib/api.ts
+++ lib/api.ts
@@ -1,19 +1,48 @@
-import { HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { ApiMapping, DomainName, HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { Architecture, Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { RetentionDays } from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
+import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { ARecord, IHostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { ApiGatewayv2DomainProperties } from 'aws-cdk-lib/aws-route53-targets';
 
 export interface ApiProps {
     table: ITable;
+    hosted_zone: IHostedZone
 }
 
 export class Api extends Construct {
     constructor(scope: Construct, id: string, props: ApiProps) {
         super(scope, id)
 
+        const apiUrl = `api.${props.hosted_zone.zoneName}`
+
         const api = new HttpApi(this, 'AnalyticsPlatformApi')
+        const certificate = new Certificate(this, 'Certificate', {
+            domainName: apiUrl,
+            validation: CertificateValidation.fromDns(props.hosted_zone)
+        })
+
+        const domainName = new DomainName(this, 'ApiDomainName', {
+            domainName: apiUrl,
+            certificate: certificate,
+        })
+
+        new ARecord(this, 'CustomDomainAliasRecord', {
+            zone: props.hosted_zone,
+            recordName: apiUrl,
+            target: RecordTarget.fromAlias(new ApiGatewayv2DomainProperties(
+                domainName.regionalDomainName,
+                domainName.regionalHostedZoneId)
+            )
+        });
+
+        new ApiMapping(this, 'ApiMapping', {
+            api: api,
+            domainName: domainName
+        })
 
         const authFunction = new Function(this, 'AuthFunction', {
             description: 'Auth endpoint for use by API Gateway',
